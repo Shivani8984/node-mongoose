328082a23f4669626bc6770ef1e08820fc9775d5
Shivani8984 <shivani8984@gmail.com>
2021-01-07 15:48:08 -0500

    Mongoose Part 3

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose'); //importing mongoose
 const Schema = mongoose.Schema; //making a shorthand to the mongoose.schema function for us so that we can just refer to it as schema
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({ //instantiates a new object named campsite schema
     name: {
         type: String,
@@ -10,7 +29,8 @@ const campsiteSchema = new Schema({ //instantiates a new object named campsite s
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, { //this second argument is used for various configuration options
     timestamps: true //automatically add 2 properties called (created at) & (updated at)
 });
